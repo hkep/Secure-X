6479414b5b6cb573ca50cdd440eec9faa6bf4ec9
curse <58047023+cursecodes@users.noreply.github.com>
2020-11-27 10:44:09 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,3 @@
-wait(0.25)
-print("Squirrel Plus Enabled. Stay safe :)")
 local s = require(game.Workspace["Squirrel+"].Settings)
 game.ReplicatedStorage.event_121134.OnServerEvent:Connect(function(player,localspeed,authcode)
 		if authcode=="SECRETCODEGENERATEAGUIDANDPUTITHERE" then
