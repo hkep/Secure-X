1860675f853613a35f8fb173fc92614abf1061cc
Void-Frost <49461342+Void-Frost@users.noreply.github.com>
2020-11-27 12:55:13 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,19 +1,17 @@
 local s = require(game.Workspace["Squirrel+"].Settings)
 game.ReplicatedStorage.event_121134.OnServerEvent:Connect(function(player,localspeed,authcode)
-		if authcode=="SECRETCODEGENERATEAGUIDANDPUTITHERE" then
-			
-	local failvalue=0
-	for i=1,#s.WalkSpeedValues,1 do
-		wait(0.1)
-		if s.WalkSpeedValues[i]~=localspeed then
-			failvalue=failvalue+1
+	if authcode=="SECRETCODEGENERATEAGUIDANDPUTITHERE" then
+
+		local failvalue=0
+		for i=1,#s.WalkSpeedValues,1 do
+			wait(0.1)
+			if s.WalkSpeedValues[i]~=localspeed then
+				failvalue=failvalue+1
+			end
+		end
+		print("Fail value: "..failvalue)
+		if failvalue==#s.WalkSpeedValues then
+			s.CatchAction(player,"Irrational Speed")
 		end
 	end
-	print("Fail value: "..failvalue)
-	if failvalue==#s.WalkSpeedValues then
-		s.CatchAction(player,"Irrational Speed")
-	end
-			else
-			s.CatchAction(player,"RemoteEvent Exploitation")
-			end
 end)
