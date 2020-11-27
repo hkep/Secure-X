a387859a3c93f199d656afb173264a96ffe20139
Void-Frost <49461342+Void-Frost@users.noreply.github.com>
2020-11-27 14:10:53 -0600

    Create CapturePos.lua

---
+++ CapturePos.lua
@@ -0,0 +1,21 @@
+--NOT WORKING
+game.Players.PlayerAdded:Connect(function(player)
+	player.CharacterAdded:Wait()
+	local playertableX = {}
+	local playertableZ = {}
+	if player.Character:FindFirstChild("UpperTorso")~=nil then 
+		while wait(1) do
+			pcall(function() 
+		if currentpos ~=nil   then
+			local xdiff = math.abs(player.Character.UpperTorso.Position.X - currentpos.X)
+			local zdiff = math.abs(player.Character.UpperTorso.Position.Z - currentpos.Z)
+			print("xdiff: "..xdiff)
+			print("zdiff: "..zdiff)
+			table.insert(playertableX,xdiff)
+			table.insert(playertableZ,zdiff) 
+				end
+				end)
+		pcall(function()currentpos = player.Character.UpperTorso.Position end)
+		end
+	end
+	end)
