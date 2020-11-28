18204b997ef2183aca6c75bc18cfc533708b7b7f
EngiAdurite <49373598+greasemonkey123@users.noreply.github.com>
2020-11-28 20:23:41 +0200

    Update ClientSide.lua

--- ClientSide.lua
+++ ClientSide.lua
@@ -1,8 +1,9 @@
 --This client-side anti-cheat component requires on security by obscurity and is therefore HEAVILY insecure
 --However, it *might* work in SOME very specific cases if the exploiter doesn't notice it's existence.
 --this is NOT THE PRIMARY COMPONENT OF THE ANTICHEAT
-game.Players.PlayerAdded:Wait()
-game.Players.LocalPlayer.Character.Humanoid.Changed:Connect(function()
-	game.ReplicatedStorage.event_121134:FireServer(game.Players.LocalPlayer.Character.Humanoid.WalkSpeed)
-	game.ReplicatedStorage.event_121134:FireServer(game.Players.LocalPlayer.Character.Humanoid.WalkSpeed,"SECRETCODEGENERATEAGUIDANDPUTITHERE")
+playerservice = game:GetService("Players")
+playerservice.PlayerAdded:Wait()
+playerservice.LocalPlayer.Character.Humanoid.Changed:Connect(function()
+	local event = game:GetService("ReplicatedStorage"):FindFirstChild("event_121134")
+	event:FireServer(game.Players.LocalPlayer.Character.Humanoid.WalkSpeed,"SECRETCODEGENERATEAGUIDANDPUTITHERE")
 end)
