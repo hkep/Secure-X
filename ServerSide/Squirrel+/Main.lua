55f96360506ca2ab16d3260ed72b6033a2599325
Void-Frost <49461342+Void-Frost@users.noreply.github.com>
2020-11-29 15:41:04 -0600

    Create Main.lua

---
+++ ServerSide/Squirrel+/Main.lua
@@ -0,0 +1,20 @@
+local event = game:GetService("ReplicatedStorage"):FindFirstChild("event_121134")
+local s = require(game.ServerScriptService["Squirrel+"].Settings)
+if event then
+	event.OnServerEvent:Connect(function(player,localspeed,authcode)
+		if authcode=="SECRETCODEGENERATEAGUIDANDPUTITHERE" then
+
+			local failvalue=0
+			for i=1,#s.WalkSpeedValues,1 do
+				wait(0.1)
+				if s.WalkSpeedValues[i]~=localspeed then
+					failvalue=failvalue+1
+				end
+			end
+			print("Fail value: "..failvalue)
+			if failvalue==#s.WalkSpeedValues then
+				s.CatchAction(player,"Irrational Speed")
+			end
+		end
+	end)
+end
