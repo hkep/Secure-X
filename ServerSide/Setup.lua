8517f9122f73ec5004e3ba209113395b0a363266
EngiAdurite <49373598+greasemonkey123@users.noreply.github.com>
2020-11-29 22:25:45 +0200

    Update Setup.lua

--- ServerSide/Setup.lua
+++ ServerSide/Setup.lua
@@ -1,4 +1,4 @@
 --Sets up remote events/other things
 local event1 = Instance.new("RemoteEvent")
-event1.Parent=game.ReplicatedStorage
+event1.Parent=game:GetService("ReplicatedStorage") -- // Again; use GetService, don't call the service directly.
 event1.Name="event_121134"
