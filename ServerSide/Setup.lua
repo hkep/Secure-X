559677e814dfb8398503ff01487595fb8436fde8
Void-Frost <49461342+Void-Frost@users.noreply.github.com>
2020-11-29 14:23:00 -0600

    Update Setup.lua

--- ServerSide/Setup.lua
+++ ServerSide/Setup.lua
@@ -1,4 +1,4 @@
 --Sets up remote events/other things
-local event1 = Instance.New("RemoteEvent")
+local event1 = Instance.new("RemoteEvent")
 event1.Parent=game.ReplicatedStorage
 event1.Name="event_121134"
