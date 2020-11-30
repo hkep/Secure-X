eda3ab9422b6e20d5625a5dede0f83c581c85a37
Void-Frost <49461342+Void-Frost@users.noreply.github.com>
2020-11-30 10:54:47 -0600

    Update CapturePos.lua

--- ServerSide/Secure X/CapturePos.lua
+++ ServerSide/Secure X/CapturePos.lua
@@ -1,17 +1,17 @@
 --NOT WORKING
-game:GetService("Players").PlayerAdded:Connect(function(Player)
-    local Player_Position
-    local X_Difference
-    local Z_Difference
-    Player.CharacterAdded:Connect(function(Character)
-    local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
-        while wait(1) do
-            if HumanoidRootPart then
-                X_Difference = math.abs(HumanoidRootPart.Position.X - CurrentPosition.X)
-                Z_Difference = math.abs(HumanoidRootPart.Position.Z - CurrentPosition.Z)
-                print(Player.Name ..": X Position Difference: " .. X_Difference .. "\n" .. Player.Name ..": Z Position Difference: " .. Z_Difference)
-                Player_Position = HumanoidRootPart.Position
-            end
-        end
-    end)
-end)
+game:GetService("Players").PlayerAdded:Connect(function(player)
+	player.CharacterAdded:Wait()
+	if player.Character:FindFirstChild("UpperTorso")~=nil then 
+		while wait(1) do
+			pcall(function() 
+		if currentpos ~=nil   then
+			local xdiff = math.abs(player.Character.UpperTorso.Position.X - currentpos.X)
+			local zdiff = math.abs(player.Character.UpperTorso.Position.Z - currentpos.Z)
+			print("xdiff: "..xdiff)
+			print("zdiff: "..zdiff)
+				end
+				end)
+		pcall(function()currentpos = player.Character.UpperTorso.Position end)
+		end
+	end
+	end)
