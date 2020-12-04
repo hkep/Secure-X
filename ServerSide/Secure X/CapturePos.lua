7f907fb2b583a9287625e9c84864a89421a99e80
hkep <75505265+hkep@users.noreply.github.com>
2020-12-04 15:57:53 -0600

    Update CapturePos.lua

--- ServerSide/Secure X/CapturePos.lua
+++ ServerSide/Secure X/CapturePos.lua
@@ -1,17 +1,32 @@
---NOT WORKING
-game:GetService("Players").PlayerAdded:Connect(function(player)
-	player.CharacterAdded:Wait()
-	if player.Character:FindFirstChild("UpperTorso")~=nil then 
+local Players = game:GetService("Players")
+local AntiTP = false
+local MaximumXZChange = 20
+-- 16 walkspeed is 16 studs/sec
+-- if you ragdoll too far it will kick you
+
+Players.PlayerAdded:Connect(function(Player)
+	if not AntiTP then return end -- don't do anything if AntiTP is false
+	
+	Player.CharacterAdded:Connect(function(Character)
+		local HRP = Character:WaitForChild("HumanoidRootPart")
+		local currentpos
+		local max_diff = MaximumXZChange^2 -- yes square it
 		while wait(1) do
 			pcall(function() 
-		if currentpos ~=nil   then
-			local xdiff = math.abs(player.Character.UpperTorso.Position.X - currentpos.X)
-			local zdiff = math.abs(player.Character.UpperTorso.Position.Z - currentpos.Z)
-			print("xdiff: "..xdiff)
-			print("zdiff: "..zdiff)
+				if currentpos~=nil then
+					local xdiff = math.abs(HRP.Position.X - currentpos.X)
+					local zdiff = math.abs(HRP.Position.Z - currentpos.Z)
+					local current_diff = xdiff*xdiff + zdiff*zdiff -- no math.sqrt
+					print("xdiff: "..xdiff)
+					print("zdiff: "..zdiff)
+					if (current_diff > max_diff) then
+						Player:Kick("Teleporting Detected")
+					end
 				end
-				end)
-		pcall(function()currentpos = player.Character.UpperTorso.Position end)
+			end)
+			pcall(function()
+				currentpos = HRP.Position
+			end)
 		end
 	end
-	end)
+end)
